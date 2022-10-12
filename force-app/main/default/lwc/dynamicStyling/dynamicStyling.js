c3e2b26b255490bb5d0f785884607ecde7eea8bf
Seda Gul <sedagulusa@gmail.com>
2022-10-12 17:00:37 -0400

    Third commit

--- force-app/main/default/lwc/dynamicStyling/dynamicStyling.js
+++ force-app/main/default/lwc/dynamicStyling/dynamicStyling.js
@@ -2,7 +2,7 @@ import { LightningElement } from 'lwc';
 
 export default class DynamicStyling extends LightningElement {
     percent=30
-    selected;
+    //selected;
     changeHandler(event){
       this.percent=event.target.value
     }
