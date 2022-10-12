58c77aecf65ccdbaf906837cac46eb619b227b14
Seda Gul <sedagulusa@gmail.com>
2022-10-12 16:37:27 -0400

    removed get method and updated style

--- force-app/main/default/lwc/dynamicStyling/dynamicStyling.js
+++ force-app/main/default/lwc/dynamicStyling/dynamicStyling.js
@@ -6,7 +6,4 @@ export default class DynamicStyling extends LightningElement {
     changeHandler(event){
       this.percent=event.target.value
     }
-    get percentage(){
-        return `width:${this.percent}%`
-    }
 }
